baff4c9f98e4518e64f0af23fcc0b1a95addfeec
Yasuhiro Yamada <yasuhiro.yamada@hacomono.co.jp>
2024-11-14 16:57:21 +0900

    ↓のような warning を黙らせるために @babel/plugin を追加  WARN  Though the "loose" option was set to "false" in your @babel/preset-env config, it will not be used for @babel/plugin-transform-private-property-in-object since the "loose" mode option was set to "true" for @babel/plugin-transform-private-methods. The "loose" option must be the same for @babel/plugin-transform-class-properties, @babel/plugin-transform-private-methods and @babel/plugin-transform-private-property-in-object (when they are enabled): you can silence this warning by explicitly adding 	["@babel/plugin-transform-private-property-in-object", { "loose": true }] to the "plugins" section of your Babel config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,14 @@ export default {
   ],
   build: {
     cache: true,
+
+    babel: {
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }],
+        ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+      ]
+    },
+
     extend (config, ctx) {
     }
   }
